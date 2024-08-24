326a786b6cf7130c79fa96ec318b5a77d49a8168
Nishworl <ashtikarnidhi19@gmail.com>
2024-08-24 21:09:25 +0530

    Adding

--- ec2/security_group.tf
+++ ec2/security_group.tf
@@ -1,21 +1,21 @@
-resource "aws_security_group" "security_group" {
-  name   = "security_group"
+# resource "aws_security_group" "security_group" {
+#   name   = "security_group"
 
-  tags = {
-    Name = "My-Server-sg"
-  }
+#   tags = {
+#     Name = "My-Server-sg"
+#   }
 
-  ingress {
-    from_port   = var.port
-    to_port     = var.port
-    protocol    = var.protocol #"tcp"
-    cidr_blocks = var.cidr_blocks #["0.0.0.0/0"]
-  }
+#   ingress {
+#     from_port   = var.port
+#     to_port     = var.port
+#     protocol    = var.protocol #"tcp"
+#     cidr_blocks = var.cidr_blocks #["0.0.0.0/0"]
+#   }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+# }
